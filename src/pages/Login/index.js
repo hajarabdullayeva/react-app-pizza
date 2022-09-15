06a2f96c30e74737a25f3a465a6858c2c9248f21
HajarAb <hadjar.abdullayeva@gmail.com>
2022-09-15 12:24:57 +0400

    navigate added

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,16 +3,23 @@ import * as yup from "yup";
 import {yupResolver} from "@hookform/resolvers/yup";
 
 
-import { registerSchema } from "../../schema/formSchema";
+import {registerSchema} from "../../schema/formSchema";
+import {useNavigate} from "react-router-dom";
 
 const textInputClassName =
     "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    function login() {
+        navigate("/");
+    }
+
     const {
         register,
         handleSubmit,
-        formState: { errors },
+        formState: {errors},
     } = useForm({
         resolver: yupResolver(registerSchema),
     });
@@ -70,6 +77,7 @@ const Login = () => {
                 <button
                     type="submit"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    onClick={login}
                 >
                     Login
                 </button>
