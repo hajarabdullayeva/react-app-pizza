c0dc1a9088701835a2148ebac9280fc12dba1fd6
HajarAb <hadjar.abdullayeva@gmail.com>
2022-09-15 10:44:46 +0400

    router added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import Index from "./components/Header";
+import "./style/style.scss"
+import {Routes, Route, useParams} from "react-router-dom";
+import {routes} from "./routes"
 function App() {
     return (
         <div className="App">
+            <Index/>
+
+            {routes.map(route => (
+                <Routes>
+                    <Route path={route.path} element={route.components}>
+                    </Route>
+                </Routes>
+            ))}
 
         </div>
     );
